f0d5904c1d0d2c6f3d35f7961f5c0522fa541cc0
MGriesbach <MGriesbach@gmail.com>
2009-02-17 11:30:27 +0100

    Translation de->en

--- lib/unziponserver_bz2.sh
+++ lib/unziponserver_bz2.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #$Id: unziponserver.sh,v 1.2 2008/05/02 08:39:41 mgr2 Exp $
+echo "Decompressing ${zipfile}"
 bunzip2 ${zipfile}
+echo "Extracting ${zipfile}"
 tar -xf ${modulname}
+echo "Removing ${zipfile}"
 rm ${modulname}
--- lib/unziponserver_zip.sh
+++ lib/unziponserver_zip.sh
@@ -2,5 +2,5 @@
 #$Id: unziponserver.sh,v 1.3 2007/01/12 11:58:25 mgr2 Exp $
 echo "Unzipping ${zipfile}"
 unzip -oq ${zipfile}
-echo "Deleting ${zipfile}"
+echo "Removing ${zipfile}"
 rm -f ${zipfile}
