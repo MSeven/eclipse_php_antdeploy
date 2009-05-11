9cbd78124e8cf958cd16ba61e708e6bbff73806a
MGriesbach <MGriesbach@gmail.com>
2009-05-11 16:43:29 +0200

    Simplofied tar.bz2 decompression script

--- lib/unziponserver_bz2.sh
+++ lib/unziponserver_bz2.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #$Id: unziponserver.sh,v 1.2 2008/05/02 08:39:41 mgr2 Exp $
-echo "Decompressing ${zipfile}"
-bunzip2 ${zipfile}
 echo "Extracting ${zipfile}"
-tar -xf ${modulname}
+tar -xjf ${zipfile}
 echo "Removing ${zipfile}"
-rm ${modulname}
+rm ${zipfile}
